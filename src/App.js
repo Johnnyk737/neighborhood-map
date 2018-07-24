12fa4a4da217ce18cf9c4910bcf1b06e2ecdba84
John Kroll <johnnyk737@gmail.com>
2018-07-23 22:24:53 -0500

    Updating readme and adding google maps api call

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let map;
+
+function initMap() {
+
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 51.5073509, lng: -0.1277582999},
+      zoom: 13
+    });
+  console.log(map);
+
+  var marker = new google.maps.Marker({position: map.center, map: map});
+}
+
 class App extends Component {
   render() {
     return (
